26a1c9b8f834212dfa87c2cd0d15d8113ea8c4dc
trandung09 <trandung09082004@gmail.com>
2024-10-29 07:33:24 +0700

    Update category view component.

---
+++ organic-food-store/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using organic_food_store.Models;
+
+namespace organic_food_store.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly OrganicFoodStoreContext _dbContext;
+
+        public CategoryViewComponent(OrganicFoodStoreContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = _dbContext.LoaiSps.ToList();
+
+            return View("Index", categories);
+        }
+    }
+}
